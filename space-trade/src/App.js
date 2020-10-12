6b3d73150523649d1f747c2f38c8971030560b7e
Pablo Pereira <invict1@outlook.es>
2020-10-11 23:13:07 -0300

    no message

--- space-trade/src/App.js
+++ space-trade/src/App.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 import { Container, Typography, TextField, Button } from "@material-ui/core";
 import { ApolloProvider } from "@apollo/react-hooks";
@@ -8,25 +7,33 @@ import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import Welcome from "./components/Welcome";
 import MenuBar from "./components/MenuBar"
-import Home from "./components/Home"
+//import Home from "./components/Home"
 import Dashboard from "./components/dashboard/Dashboard"
 import Stock from "./components/stock/Stock"
 import ChatBot from "./components/chatbot/chatbotTheme";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useLocation, useState } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from "./assets/logo.png";
 import { ModalManager } from "@material-ui/core";
+import React, { useRef, useEffect } from 'react';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
-const Logo = styled.img`
-  width: 100px;
-  height: 100px;
-  object-fit: contain;
-`;
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views 
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
 
 var stocks = [
   {
@@ -41,27 +48,63 @@ var stocks = [
   }
 ];
 
-function App() {
+const App = () => {
   localStorage.setItem('balance', 5000);
   localStorage.setItem('stocks', JSON.stringify(stocks));
 
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Route path="/" component={MenuBar} />
-        <Route exact path="/" component={Home} />
-        <Switch>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/stocks/:stockId" component={Stock} />
-          <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/register" component={RegisterForm} />
-        </Switch>
-        <ChatBot />
-      </Router>
-    </ApolloProvider>
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <ApolloProvider client={client}>
+          <Switch>
+            <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+
+            <Route path="/dashboard">
+              <MenuBar />
+              <Dashboard />
+            </Route>
+            
+            <Route path="/stocks/:stockId">
+              <MenuBar />
+              <Stock/>
+            </Route>
+            
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/register" component={RegisterForm} />
+
+          </Switch>
+          <ChatBot />
+        </ApolloProvider>
+      )} />
   );
 }
 
 export default App;
+
+
+/*
+function App() {
+  return (
+      <Router>
+        <Route path="/" component={MenuBar} />
+        <Route exact path="/" component={Home} />
+        <Switch>
+
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;*/
--- space-trade/src/index.js
+++ space-trade/src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import './App.css';
+import './assets/scss/style.scss';
+
+import './index.css';
+import "./stylesheet/main.sass";
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "./stylesheet/main.sass";
+import './assets/scss/style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,4 +45,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();*/
