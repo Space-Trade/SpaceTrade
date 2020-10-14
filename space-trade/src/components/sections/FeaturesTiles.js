10f862802c9df414ec9fd933bd87f9babb3889c1
Pablo Pereira <invict1@outlook.es>
2020-10-14 15:39:32 -0300

    no message

--- space-trade/src/components/sections/FeaturesTiles.js
+++ space-trade/src/components/sections/FeaturesTiles.js
@@ -44,8 +44,7 @@ const FeaturesTiles = ({
   );
 
   const sectionHeader = {
-    title: 'Build up the whole picture',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+    title: 'Build up the whole picture'
   };
 
   return (
